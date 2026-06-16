dc22044631eaa94498a6a396808b6aa185c8ddb7
Vo Anh Hao <email_github_cua_ban@gmail.com>
2026-06-16 13:56:43 +0700

    public to github

---
+++ GITHUB/testgit.java
@@ -0,0 +1,15 @@
+package first.GITHUB;
+import java.util.Scanner;
+public class testgit {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a ;
+        int b ;
+       Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the value of a: ");
+        a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
+        b = sc.nextInt();
+        System.out.println("The sum of a and b is: " + (a+b));
+    }
+}
